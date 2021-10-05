70a1e57d99d2e0eca9b783a7009e8f23421371f3
kise <kaiazmi1412@gmail.com>
2021-10-06 00:32:49 +0530

    WhiteSpace in Template Literal & Normal String

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,7 +2,9 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 currentResult = currentResult + 10 * 3 / 2 - 1;
-
+// Template literal
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDescription);
+let errorMessage = 'An error \n' +
+                    'occured!';
+// Normal String with whitespace
+outputResult(currentResult, errorMessage);
