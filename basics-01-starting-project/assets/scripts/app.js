351837d9a6fcb729b8812a8661d09d6bc9618f4c
kise <kaiazmi1412@gmail.com>
2021-10-07 23:18:50 +0530

    return Keyword

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,15 +1,13 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// Return Implimentation in Function
 function add(num1, num2) {
     const result = num1 + num2;
-    alert('The result is ' + result);
+    return result;     
 }
 
-add(1, 2);
-add(5, 5);
-
-currentResult = currentResult + 10 * 3 / 2 - 1;
+currentResult = add(1, 2);
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
