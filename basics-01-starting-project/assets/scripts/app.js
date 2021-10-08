d16f7884b1812e6149f2b5e9e0b1b38b8e502b5b
kise <kaiazmi1412@gmail.com>
2021-10-08 20:12:01 +0530

    Executing Functions "Indirectly"

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -3,12 +3,8 @@ let currentResult = defaultResult;
 
 // Return Implimentation in Function
 function add(num1, num2) {
-    const result = num1 + num2;
-    return result;     
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, '');
 }
 
-currentResult = add(1, 2);
-
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener('click', add);
