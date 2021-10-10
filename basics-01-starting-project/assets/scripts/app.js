f62c650c74b8d415e97f2ce238a9d0345ade8803
kise <kaiazmi1412@gmail.com>
2021-10-10 18:10:20 +0530

    #1 Splitting Codes into Functions

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,12 +1,11 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-// Converting String to Integer usnig parseFloat & parseInt for specific types
+// Splitting Codes into Functions 
 function add() {
-// currentResult = currentResult + +userInput.value;  (+ gives Decimal num & Integers)
-// currentResult = currentResult.toString + userInput.value;  (toString convert to String -- less useage)
+    const calcDescription = `${currentResult} + ${userInput.value}`;
     currentResult = currentResult + parseInt(userInput.value);
-    outputResult(currentResult, '');
+    outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
