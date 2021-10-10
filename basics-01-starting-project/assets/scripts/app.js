4d69a561ae7a245aa7e508d14e4bde0c98f4816f
kise <kaiazmi1412@gmail.com>
2021-10-10 18:21:56 +0530

    #3 Splitting Codes into Functions

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,11 +2,16 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 // Splitting Codes into Functions
+
+function getUserNumberInput() {
+  return parseInt(usrInput.value);
+}
+
 function add() {
-    const enteredNumber = parseInt(usrInput.value)
-    const calcDescription = `${currentResult} + ${enteredNumber.value}`;
-    currentResult = currentResult + enteredNumber.value;
-    outputResult(currentResult, calcDescription);
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} + ${enteredNumber.value}`;
+  currentResult = currentResult + enteredNumber.value;
+  outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
