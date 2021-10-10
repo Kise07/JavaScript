6fc670bd4904267e2668380a6410500ba8d66fc6
kise <kaiazmi1412@gmail.com>
2021-10-10 21:14:29 +0530

    Shorthands Operators (+=, -=, *=, /=, ++ & --)

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,43 +1,41 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-// This is a comment: (//) & (/* */)
-// Gets inputs from input field
 function getUserNumberInput() {
   return parseInt(usrInput.value);
 }
 
-// Generates and writes calculation log
 function createAndWriteOutput(operator, resultBeforeCalc, CalcNumber) {
   const calcDescription = `${resultBeforeCalc} ${operator} ${CalcNumber}`;
-  outputResult(currentResult, calcDescription); // from vender file
+  outputResult(currentResult, calcDescription);
 }
 
+// Shorthand Operators 
 function add() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult + enteredNumber;
+  currentResult += enteredNumber; // Shorthand
   createAndWriteOutput('+', initialResult, enteredNumber);
 }
 
 function subtract() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult - enteredNumber;
+  currentResult -= enteredNumber; // Shorthand
   createAndWriteOutput('-', initialResult, enteredNumber);
 }
 
 function multiply() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult * enteredNumber;
+    currentResult *= enteredNumber; // Shorthand
     createAndWriteOutput('*', initialResult, enteredNumber);
   }
 
   function divide() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult / enteredNumber;
+    currentResult /= enteredNumber; // Shorthand
     createAndWriteOutput('/', initialResult, enteredNumber);
   }
   
