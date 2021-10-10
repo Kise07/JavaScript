4e31f80b6013983567e7c50909c130364c591dc1
kise <kaiazmi1412@gmail.com>
2021-10-10 18:17:41 +0530

    #2 Splitting Codes into Functions

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,7 +1,7 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-// Splitting Codes into Functions 
+// Splitting Codes into Functions
 function add() {
     const enteredNumber = parseInt(usrInput.value)
     const calcDescription = `${currentResult} + ${enteredNumber.value}`;
