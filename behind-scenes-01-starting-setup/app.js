f6b981660c372bfc1d7977b1d3fe249a0cec4058
kise <kaiazmi1412@gmail.com>
2021-10-21 16:18:48 +0530

    var vs let vs const - Introducing 'Block Scope'

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,19 @@
+let name = 'Max';
+let hobbies; // Global Variables using let
+
+if (name === 'Max') {
+  //   var hobbies = ['Sports', 'Cooking']; //Global variable within array
+  //   let hobbies = ['Sports', 'Cooking']; //local variable within array
+  hobbies = ['Sports', 'Cooking']; //Using Global variable within
+  console.log(hobbies);
+}
+
+function greet() {
+  let age = 30; // local Variable
+  let name = 'Manuel'; // shadow variable
+  console.log(name, age, hobbies);
+}
+
+console.log(name, hobbies);
+
+greet();
