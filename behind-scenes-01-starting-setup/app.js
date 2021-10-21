1e9710e572fc1271e0cd60c83e43d4ef28e26c2c
kise <kaiazmi1412@gmail.com>
2021-10-21 16:37:48 +0530

    understanding - 'Hoisting'

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -1,19 +1,23 @@
-let name = 'Max';
-let hobbies; // Global Variables using let
+// let name = 'Max';
+// let hobbies; // Global Variables using let
 
-if (name === 'Max') {
-  //   var hobbies = ['Sports', 'Cooking']; //Global variable within array
-  //   let hobbies = ['Sports', 'Cooking']; //local variable within array
-  hobbies = ['Sports', 'Cooking']; //Using Global variable within
-  console.log(hobbies);
-}
+// if (name === 'Max') {
+//   //   var hobbies = ['Sports', 'Cooking']; //Global variable within array
+//   //   let hobbies = ['Sports', 'Cooking']; //local variable within array
+//   hobbies = ['Sports', 'Cooking']; //Using Global variable within
+//   console.log(hobbies);
+// }
 
-function greet() {
-  let age = 30; // local Variable
-  let name = 'Manuel'; // shadow variable
-  console.log(name, age, hobbies);
-}
+// function greet() {
+//   let age = 30; // local Variable
+//   let name = 'Manuel'; // shadow variable
+//   console.log(name, age, hobbies);
+// }
 
-console.log(name, hobbies);
+// console.log(name, hobbies);
 
-greet();
+// greet();
+
+console.log(userName);
+
+var userName = 'Max'; // Hoisting
