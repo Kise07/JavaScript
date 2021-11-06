087d3c4edad5205cdb953573b4a52d75be72dcb5
kise <kaiazmi1412@gmail.com>
2021-11-06 23:58:04 +0530

    Which Data Can You Store In Arrays?

--- arrays-01-starting-code/app.js
+++ arrays-01-starting-code/app.js
@@ -1,3 +1,4 @@
+// #1 Creating Array
 const numbers = [1, 2, 3];
 console.log(numbers);
 
@@ -13,3 +14,21 @@ console.log(listItems);
 
 const arrayListItems = Array.from(listItems);
 console.log(arrayListItems);
+
+// #2 Data in Array
+const hobbies = ['Cooking', 'Sports']; // Simple Array
+const personalData = [30, 'Max', { moreDetails: [] }]; // Mixed Types
+
+const analyticsData = [
+  [1, 1.6],
+  [-5.4, 2.1],
+]; // array in Array Or Multi-dimensional
+
+for (const data of analyticsData) {
+  // nested Arrays
+  for (const dataPoint of data) {
+    console.log(dataPoint);
+  }
+}
+
+console.log(personalData[1]); // Indexing personalData
