148cc775df66d2dfa1ab80637755c7d9906fae1c
kise <kaiazmi1412@gmail.com>
2021-11-07 20:38:59 +0530

    Selecting Ranges & Creating Copies with slice()

--- arrays-01-starting-code/app.js
+++ arrays-01-starting-code/app.js
@@ -1,62 +1,72 @@
-// // #1 Creating Array
-// const numbers = [1, 2, 3];
-// console.log(numbers);
+// // // #1 Creating Array
+// // const numbers = [1, 2, 3];
+// // console.log(numbers);
 
-// // // const moreNumbers = new Array(5, 2);
-// // const moreNumbers = Array(5, 2);
-// // console.log(moreNumbers);
+// // // // const moreNumbers = new Array(5, 2);
+// // // const moreNumbers = Array(5, 2);
+// // // console.log(moreNumbers);
 
-// // const yetMoreNumbers = Array.of(1, 2);
-// // console.log(yetMoreNumbers);
+// // // const yetMoreNumbers = Array.of(1, 2);
+// // // console.log(yetMoreNumbers);
 
-// const listItems = document.querySelectorAll('li');
-// console.log(listItems);
+// // const listItems = document.querySelectorAll('li');
+// // console.log(listItems);
 
-// const arrayListItems = Array.from(listItems);
-// console.log(arrayListItems);
+// // const arrayListItems = Array.from(listItems);
+// // console.log(arrayListItems);
 
-// // #2 Data in Array
-// const hobbies = ['Cooking', 'Sports']; // Simple Array
-// const personalData = [30, 'Max', { moreDetails: [] }]; // Mixed Types
+// // // #2 Data in Array
+// // const hobbies = ['Cooking', 'Sports']; // Simple Array
+// // const personalData = [30, 'Max', { moreDetails: [] }]; // Mixed Types
 
-// const analyticsData = [
-//   [1, 1.6],
-//   [-5.4, 2.1],
-// ]; // array in Array Or Multi-dimensional
+// // const analyticsData = [
+// //   [1, 1.6],
+// //   [-5.4, 2.1],
+// // ]; // array in Array Or Multi-dimensional
 
-// for (const data of analyticsData) {
-//   // nested Arrays
-//   for (const dataPoint of data) {
-//     console.log(dataPoint);
-//   }
-// }
+// // for (const data of analyticsData) {
+// //   // nested Arrays
+// //   for (const dataPoint of data) {
+// //     console.log(dataPoint);
+// //   }
+// // }
 
-// console.log(personalData[1]); // Indexing personalData
+// // console.log(personalData[1]); // Indexing personalData
 
-// Adding & Removing Elements--
-const hobbies = ['Sports', 'Cooking']; // An Array
-hobbies.push('Reading'); // Adding a Element from behind
-hobbies.unshift('Coding'); // Adding a Element from front
-const poppedValue = hobbies.pop(); // Removing a Element & Storing The Value
-hobbies.shift(); // Removing a Element from front
-console.log(hobbies);
-
-hobbies[1] = 'Coding'; // Shorthand for Add or Replace
-// hobbies[5] = 'Reading'; // rarely used
-console.log(hobbies, hobbies[4]);
-
-// Splice Method()--
-// hobbies.splice(0, 0, 'Good Food'); // Adding a Element Within
+// // Adding & Removing Elements--
+// const hobbies = ['Sports', 'Cooking']; // An Array
+// hobbies.push('Reading'); // Adding a Element from behind
+// hobbies.unshift('Coding'); // Adding a Element from front
+// const poppedValue = hobbies.pop(); // Removing a Element & Storing The Value
+// hobbies.shift(); // Removing a Element from front
 // console.log(hobbies);
 
-hobbies.splice(1, 0, 'Good Food'); // Adding a Element Within index[1]
-console.log(hobbies);
+// hobbies[1] = 'Coding'; // Shorthand for Add or Replace
+// // hobbies[5] = 'Reading'; // rarely used
+// console.log(hobbies, hobbies[4]);
 
-// hobbies.splice(0, 1, 'Good Food'); // Removing a Element Within index[1]
+// // Splice Method()--
+// // hobbies.splice(0, 0, 'Good Food'); // Adding a Element Within
+// // console.log(hobbies);
+
+// hobbies.splice(1, 0, 'Good Food'); // Adding a Element Within index[1]
 // console.log(hobbies);
 
-// hobbies.splice(0); // Removing whole array
+// // hobbies.splice(0, 1, 'Good Food'); // Removing a Element Within index[1]
+// // console.log(hobbies);
+
+// // hobbies.splice(0); // Removing whole array
+// // console.log(hobbies);
+
+// const removeElements = hobbies.splice(-1, 1); // Storing Removed Element
 // console.log(hobbies);
 
-const removeElements = hobbies.splice(-1, 1); // Storing Removed Element
-console.log(hobbies);
+// slice Method()--
+const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+// const storedResults = testResults.slice(0, 2); // Start & End
+// const storedResults = testResults.slice(-3, -1); // -N S & -N E
+const storedResults = testResults.slice(2); // Start to End
+
+testResults.push(5.91);
+
+console.log(storedResults, testResults);
