43e06539faaf36cd7a51215453b1efb565ed61be
kise <kaiazmi1412@gmail.com>
2021-11-09 02:05:03 +0530

    sort()ing and reverse()ing methods

--- arrays-01-starting-code/app.js
+++ arrays-01-starting-code/app.js
@@ -125,4 +125,28 @@ const taxAdjustedPrices = prices.map((price, idx, prices) => {
   return priceObj;
 });
 
-console.log(prices, taxAdjustedPrices);
+// console.log(prices, taxAdjustedPrices);
+
+// sort() & reverse() method--
+// const sortedPrices = prices.sort((a, b) => { // Sort()
+//   if (a > b) {
+//     return 1;
+//   } else if (a === b) {
+//     return 0;
+//   } else {
+//     return -1;
+//   }
+// });
+
+const sortedPrices = prices.sort((a, b) => {
+  // Reverse()
+  if (a > b) {
+    return -1;
+  } else if (a === b) {
+    return 0;
+  } else {
+    return 1;
+  }
+});
+
+console.log(sortedPrices); // or .reverse()
