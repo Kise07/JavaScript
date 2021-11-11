1abdc6656cf9b9b6a653eec7c1b391bfc0aa4f43
kise <kaiazmi1412@gmail.com>
2021-11-11 22:08:01 +0530

    Working with Maps

--- arrays-01-starting-code/setsmaps.js
+++ arrays-01-starting-code/setsmaps.js
@@ -1,9 +1,44 @@
-const ids = new Set(['Hi', 'from', 'set!']);
-ids.add(2);
-if (ids.has('Hi')) {
-  ids.delete('Hi');
+// Working with Sets--
+// const ids = new Set(['Hi', 'from', 'set!']); // Initialize Set w new()
+// ids.add(2);
+// if (ids.has('Hi')) {
+//   ids.delete('Hi');
+// }
+
+// for (const entry of ids.entries()) {
+//   console.log(entry[0]);
+// }
+
+// Working with Maps--
+const person1 = { name: 'Max' }; // Normal Objects
+const person2 = { name: 'Manuel' };
+
+// Initialize Map w new()
+const personData = new Map([[person1, [{ date: 'yesterday', price: 10 }]]]);
+
+personData.set(person2, [{ date: 'two weeks ago', price: 100 }]);
+
+console.log(personData);
+console.log(personData.get(person1));
+
+// for (const entry of personData.entries()) { // #1 Normal
+//   console.log(entry);
+// }
+
+// #2 Using Array Destructuring--
+for (const [key, value] of personData.entries()) {
+  console.log(key, value);
 }
 
-for (const entry of ids.entries()) {
-  console.log(entry[0]);
+// #3 Using Keys function--
+for (const key of personData.keys()) {
+  console.log(key);
 }
+
+// #4 Using Values function--
+for (const value of personData.values()) {
+  console.log(value);
+}
+
+// Size Properties--
+console.log(personData.size);
