942b60948d00426e9b496a60b72041110bacda41
kise <kaiazmi1412@gmail.com>
2021-11-12 21:23:38 +0530

    Understanding WeakSet

--- arrays-01-starting-code/setsmaps.js
+++ arrays-01-starting-code/setsmaps.js
@@ -10,35 +10,45 @@
 // }
 
 // Working with Maps--
-const person1 = { name: 'Max' }; // Normal Objects
-const person2 = { name: 'Manuel' };
+// const person1 = { name: 'Max' }; // Normal Objects
+// const person2 = { name: 'Manuel' };
 
 // Initialize Map w new()
-const personData = new Map([[person1, [{ date: 'yesterday', price: 10 }]]]);
+// const personData = new Map([[person1, [{ date: 'yesterday', price: 10 }]]]);
 
-personData.set(person2, [{ date: 'two weeks ago', price: 100 }]);
+// personData.set(person2, [{ date: 'two weeks ago', price: 100 }]);
 
-console.log(personData);
-console.log(personData.get(person1));
+// console.log(personData);
+// console.log(personData.get(person1));
 
 // for (const entry of personData.entries()) { // #1 Normal
 //   console.log(entry);
 // }
 
 // #2 Using Array Destructuring--
-for (const [key, value] of personData.entries()) {
-  console.log(key, value);
-}
+// for (const [key, value] of personData.entries()) {
+//   console.log(key, value);
+// }
 
 // #3 Using Keys function--
-for (const key of personData.keys()) {
-  console.log(key);
-}
+// for (const key of personData.keys()) {
+//   console.log(key);
+// }
 
 // #4 Using Values function--
-for (const value of personData.values()) {
-  console.log(value);
-}
+// for (const value of personData.values()) {
+//   console.log(value);
+// }
 
 // Size Properties--
-console.log(personData.size);
+// console.log(personData.size);
+
+// Understanding WeakSet--
+let person = { name: 'Max' };
+const persons = new WeakSet();
+persons.add(person);
+
+// ... some operations
+person = null;
+
+console.log(persons);
